0b101e0dec2873ecab3a3a182a63d0bb25f3e6ae
sudhanshu-SID <sudhanshu7536@gmail.com>
2026-02-01 02:29:03 +0530

    fix the whatsapp redirect issue

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 export const handleOrder = (productName: string): void => {
-  const phone = "15550000000"; 
+  const phone = "7896409312"; 
   const message = `Hey YUMECAV, I'm ready to elevate my style. I'm interested in the "${productName}" custom order.`;
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, '_blank');
